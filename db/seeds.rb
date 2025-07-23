cb3cfc99e4b639c13bbff9334c76d309c1da5572
Monatric <nawpnawp8@gmail.com>
2025-07-23 18:33:43 +0800

    feat: add seeds for flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,62 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
+# Airport seeds
 AIRPORT_CODES = %w[SFO LAX JFK ORD DFW MIA MNL CEB CRK HND CDG DXB]
 
 AIRPORT_CODES.each do |code|
   Airport.find_or_create_by!(code: code)
 end
 
-p "Created #{Airport.count} airport codes"
+p "Seeded #{Airport.count} airport codes"
+
+# Flight seeds
+FLIGHTS = [
+  {
+    departure_airport_id: 1,
+    arrival_airport_id: 2,
+    start_date: DateTime.new(2025, 8, 10, 18, 00, 0),
+    flight_duration: 3.hours
+  },
+  {
+    departure_airport_id: 12,
+    arrival_airport_id: 5,
+    start_date: DateTime.new(2025, 8, 5, 8, 30, 0),
+    flight_duration: 2.hours + 45.minutes
+  },
+  {
+    departure_airport_id: 7,
+    arrival_airport_id: 6,
+    start_date: DateTime.new(2025, 8, 8, 15, 15, 0),
+    flight_duration: 7.hours
+  },
+  {
+    departure_airport_id: 3,
+    arrival_airport_id: 10,
+    start_date: DateTime.new(2025, 9, 1, 0, 0, 0),
+    flight_duration: 5.hours + 30.minutes
+  },
+  {
+    departure_airport_id: 3,
+    arrival_airport_id: 9,
+    start_date: DateTime.new(2025, 8, 12, 1, 30, 0),
+    flight_duration: 50.minutes
+  },
+  {
+    departure_airport_id: 9,
+    arrival_airport_id: 5,
+    start_date: DateTime.new(2025, 7, 25, 9, 0, 0),
+    flight_duration: 12.hours
+  }
+]
+
+FLIGHTS.each do |flight|
+  Flight.find_or_create_by!(
+    departure_airport_id: flight[:departure_airport_id],
+    arrival_airport_id: flight[:arrival_airport_id],
+    start_date: flight[:start_date],
+    flight_duration: flight[:flight_duration]
+  )
+end
+
+p "Seeded #{Flight.count} flights"
