62b490debf4256e34ce3ef2c8776fd4198993d1c
Monatric <nawpnawp8@gmail.com>
2025-07-23 22:54:59 +0800

    feat: validate flight columns

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,7 @@
 class Flight < ApplicationRecord
+  validates :start_date, presence: true, comparison: { greater_than: Time.now.next_day, less_than: 1.year.from_now }
+  validates :flight_duration, presence: true, comparison: { greater_than: 1.hour, less_than: 24.hours }
+
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
 end
