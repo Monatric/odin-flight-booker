ff0e5992052477ffc165c69a2fb891cab1a7f30e
Monatric <nawpnawp8@gmail.com>
2025-07-25 04:08:04 +0800

    feat: show available flights based on selected options

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,5 +4,10 @@ class FlightsController < ApplicationController
     @arrival_airports = Flight.all.map { |f| [ f.arrival_airport.code, f.arrival_airport.code ] }
     @flight_dates = Flight.dates_asc
     @num_tickets = 4.times.map { |n| [ n + 1, n + 1 ] }
+
+    departure_code = Airport.find_by(code: params[:departure_code])
+    arrival_code = Airport.find_by(code: params[:arrival_code])
+    start_date = params[:start_date].to_date.all_day
+    @flights = Flight.available_flights(departure_code, arrival_code, start_date)
   end
 end
