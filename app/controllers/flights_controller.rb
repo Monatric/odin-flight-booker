b8fbbfce7d151087615393341d5f3b721b475274
Monatric <nawpnawp8@gmail.com>
2025-07-25 03:14:04 +0800

    feat: build search form for available flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,8 @@
 class FlightsController < ApplicationController
   def index
-    @departure_airports = Flight.all.map { |flight| [ flight.departure_airport.code, flight.departure_airport.id ] }
-    @arrival_airports = Flight.all.map { |flight| [ flight.arrival_airport.code, flight.arrival_airport.id ] }
+    @departure_airports = Flight.all.map { |f| [ f.departure_airport.code, f.departure_airport.code ] }
+    @arrival_airports = Flight.all.map { |f| [ f.arrival_airport.code, f.arrival_airport.code ] }
+    @flight_dates = Flight.dates_asc
+    @num_tickets = 4.times.map { |n| [ n + 1, n + 1 ] }
   end
 end
