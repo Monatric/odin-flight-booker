bc8493158318f66919d23fa0755cede053b91639
Monatric <nawpnawp8@gmail.com>
2025-07-25 19:31:41 +0800

    refactor: permit search params & reduce flights ctrl methods code

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,13 +1,23 @@
 class FlightsController < ApplicationController
   def index
-    @departure_airports = Flight.all.map { |f| [ f.departure_airport.code, f.departure_airport.code ] }
-    @arrival_airports = Flight.all.map { |f| [ f.arrival_airport.code, f.arrival_airport.code ] }
+    @airports = Airport.pluck(:code, :code)
     @flight_dates = Flight.dates_asc
     @num_tickets = 4.times.map { |n| [ n + 1, n + 1 ] }
+    @search = flight_search_params
 
-    departure_code = Airport.find_by(code: params[:departure_code])
-    arrival_code = Airport.find_by(code: params[:arrival_code])
-    start_date = params[:start_date].to_date.all_day
-    @flights = Flight.available_flights(departure_code, arrival_code, start_date)
+    @flights = available_flights
   end
+
+  private
+
+    def available_flights
+      departure_code = Airport.find_by(code: @search[:departure_code])
+      arrival_code = Airport.find_by(code: @search[:arrival_code])
+      start_date = @search[:start_date].to_date.all_day if @search[:start_date].present?
+      Flight.available_flights(departure_code, arrival_code, start_date)
+    end
+
+    def flight_search_params
+      params.permit(:departure_code, :arrival_code, :start_date, :num_tickets)
+    end
 end
