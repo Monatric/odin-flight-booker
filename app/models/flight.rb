59d1659250495e14db7a0f9f21a44dedf305a3ec
Monatric <nawpnawp8@gmail.com>
2025-07-25 22:20:30 +0800

    refactor: update flight duration validation

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,6 @@
 class Flight < ApplicationRecord
   validates :start_date, presence: true, comparison: { greater_than: Time.now.next_day, less_than: 1.year.from_now }
-  validates :flight_duration, presence: true, comparison: { greater_than: 1.hour, less_than: 24.hours }
+  validates :flight_duration, presence: true, comparison: { greater_than: 59.minutes, less_than: 24.hours }
 
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
