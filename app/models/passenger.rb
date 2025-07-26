6530d04b7bdb7b479bac9a3402420074c04b4cf3
Monatric <nawpnawp8@gmail.com>
2025-07-27 02:44:45 +0800

    feat: generate Passenger model

---
+++ app/models/passenger.rb
@@ -0,0 +1,6 @@
+class Passenger < ApplicationRecord
+  validates :name, presence: true, length: { in: 3..100 }, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  validates :email, presence: true
+
+  belongs_to :booking
+end
