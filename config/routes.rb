40a4c72ca7bce0eaa52ebb650d1410dc4428492c
Monatric <nawpnawp8@gmail.com>
2025-07-27 20:31:09 +0800

    feat: set up routes for Bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :bookings, only: %i[ new ]
+  resources :bookings, only: %i[ new show create ]
   resources :flights, only: %i[ index ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
