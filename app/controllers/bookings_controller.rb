aeff4385bd7ca34af6990259a762a244bad914c4
Monatric <nawpnawp8@gmail.com>
2025-07-28 00:43:33 +0800

    feat: add form for booking with passengers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,5 +2,9 @@ class BookingsController < ApplicationController
   def new
     @booking = Booking.new
     @flight = Flight.find(params[:flight_id])
+    params[:num_tickets].to_i.times { @booking.passengers.build }
+  end
+
+  def create
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,6 @@ class Booking < ApplicationRecord
   validates :num_tickets, presence: true, numericality: { greater_than: 0, less_than: 5 }
 
   has_many :passengers
+  accepts_nested_attributes_for :passengers
   belongs_to :flight
 end
