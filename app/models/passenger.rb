af1f368455c62d50483eb870f9483f56ae5966ac
Monatric <nawpnawp8@gmail.com>
2025-07-28 02:52:33 +0800

    feat: add inverse_of assoc in models

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,5 +2,5 @@ class Passenger < ApplicationRecord
   validates :name, presence: true, length: { in: 3..100 }, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
   validates :email, presence: true
 
-  belongs_to :booking
+  belongs_to :booking, inverse_of: :passengers
 end
