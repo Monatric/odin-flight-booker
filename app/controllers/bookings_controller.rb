d15c77f3ab12973cee93a7584b656312786e8e9b
Monatric <nawpnawp8@gmail.com>
2025-07-28 01:24:01 +0800

    feat: add create & show actions for Bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,6 +5,24 @@ class BookingsController < ApplicationController
     params[:num_tickets].to_i.times { @booking.passengers.build }
   end
 
-  def create
+  def show
+    @booking = Booking.find(params[:id])
   end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to @booking, notice: "Flight booked successfully!"
+    else
+      flash.now[:alert] = "Flight cannot be booked."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def booking_params
+      params.expect(booking: [ :num_tickets, :flight_id, passengers_attributes: [ [ :id, :name, :email ] ] ])
+    end
 end
